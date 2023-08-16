5ca221578d08ce764643e2a56c825a5eaf8d5cf5
Mélina Verger <meverger@gmail.com>
2023-08-17 00:31:00 +0200

    create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="maddpkg",
+    version="0.0.1",
+    description="A Python package to compute the MADD metric",
+    url="https://github.com/melinaverger/MADDpkg",
+    author="Mélina Verger",
+    license="CC BY-NC 4.0",
+    classifiers=[
+        "Programming Language :: Python :: 3.10.4",
+        "Operating System :: OS Independent"
+        ],
+    install_requires=[],
+    python_requires=">=3.10.4"
+)
