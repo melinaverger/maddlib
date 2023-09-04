1ec7ebff3113b7a7f2d80572afbffb444f84dc94
Mélina Verger <meverger@gmail.com>
2023-09-04 12:01:25 +0200

    add long_description and twine package

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name="maddpkg",
     version="0.3.0",
     description="A Python package to compute the MADD metric",
+    long_description="This repository contains the source code of the Python package related to the MADD metric."
     url="https://github.com/melinaverger/MADDpkg",
     author="Mélina Verger",
     author_email="melina.verger@lip6.fr",
@@ -15,5 +16,6 @@ setup(
         "Operating System :: OS Independent"
         ],
     install_requires=["scikit-learn", "pandas", "numpy"],
+    extras_require={"dev":["twine>=4.0.2"]},
     python_requires=">=3.10.4"
 )
