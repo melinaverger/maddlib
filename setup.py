1da183cdf7584afea4e48717ab116b90a755da45
Mélina Verger <meverger@gmail.com>
2025-11-20 14:39:53 +0100

    change version (from 1.0.0 to 1.1.0)

--- setup.py
+++ setup.py
@@ -2,20 +2,20 @@ from setuptools import setup, find_packages
 
 setup(
     name="maddlib",
-    version="1.0.0",
+    version="1.1.0",
     description="A Python package to compute MADD metric-related functions",
     long_description_content_type="text/markdown",
     long_description=open('README.md').read(),
     url="https://github.com/melinaverger/maddlib",
     author="Mélina Verger",
-    author_email="melina.verger@lip6.fr",
+    author_email="melina.verger@insa-lyon.fr",
     packages=find_packages(),
     license="CC BY-NC 4.0",
     classifiers=[
         "Programming Language :: Python :: 3.10",
         "Operating System :: OS Independent"
         ],
-    install_requires=["scikit-learn", "pandas", "numpy", "seaborn", "matplotlib"],
+    install_requires=["scikit-learn", "pandas", "numpy", "scipy", "seaborn", "matplotlib"],
     extras_require={"dev":["twine>=4.0.2"]},
     python_requires=">=3.10.4"
 )
